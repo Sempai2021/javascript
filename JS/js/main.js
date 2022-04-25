96938c52dc02e2ca96a8f2201d7397b5e1199ff5
Artoym <tem4ik.capper@gmail.com>
2022-04-25 19:54:47 +0300

    add practical

--- JS/js/main.js
+++ JS/js/main.js
@@ -1,1 +1,22 @@
 "use strict";
+
+// Задание 1
+
+const numberOfFilms = prompt('Один из просмотренных фильмов?', '');
+const numberOfFilmsTwo = prompt('На сколько оцените его', '');
+const numberOfFilmsThree = prompt('Один из просмотренных фильмов?', '');
+const numberOfFilmsTFour = prompt('На сколько оцените его', '');
+// Задание 2
+
+const personaMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+personaMovieDB.movies[numberOfFilms] = numberOfFilmsTwo;
+personaMovieDB.movies[numberOfFilmsThree] = numberOfFilmsTFour;
+
+console.log(personaMovieDB);
