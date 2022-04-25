f65906f04ab5562daae9c750a63ff408be5459d4
Artoym <tem4ik.capper@gmail.com>
2022-04-26 00:51:05 +0300

    cycles

--- JS/js/main.js
+++ JS/js/main.js
@@ -1,24 +1,35 @@
 "use strict";
 
-// Циклы
-
-let num = 50;
-
-// while (num <= 55) {
-//     console.log(num);
-//     num++;
+// for (let i = 0; i < 3; i++) {
+//     console.log(i);
+//     for (let j = 0; j < 3; j++) {
+//         console.log(j);
+//     }
 // }
 
-// do {
-//     console.log(num);
-//     num++;
-// }
-// while (num <= 55);
+// let result = '';
+// const lengthResult = 7;
 
-for (let num = 50; num <= 55; num++) {
-    if (num === 53) {
-        // break;
-        continue;
+// for (let i = 1; i < lengthResult; i++) {
+
+//     for (let j = 0; j < i; j++) {
+//         result += "*";
+//     }
+
+//     result += '\n';
+// }
+
+// console.log(result);
+
+first: for (let i = 0; i < 3; i++) {
+    console.log(`First level: ${i}`);
+    for (let j = 0; j < 3; j++) {
+        console.log(`Second level: ${j}`);
+        for (let k = 0; k < 5; k++) {
+            if (k == 2) {
+                continue first;
+            }
+            console.log(`Third level: ${k}`);
+        }
     }
-    console.log(num);
 }
