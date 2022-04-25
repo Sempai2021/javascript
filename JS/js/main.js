a6112db53358a9536ccb13fc8e07eb2f2b630a5d
Artoym <tem4ik.capper@gmail.com>
2022-04-25 21:05:08 +0300

    add condition

--- JS/js/main.js
+++ JS/js/main.js
@@ -1,36 +1,70 @@
 "use strict";
 
-// if (4 == 9) {
-//     console.log('Ok');
-// } else {
-//     console.log('Error');
+// const hamburger = 5;
+// const fries = 0;
+
+// if (hamburger && fries) {
+//     console.log('Я ссыт!');
 // }
 
+// const hamburger = 3;
+// const fries = 1;
+// const cola = 1;
 
+// console.log(hamburger === 3 && cola && fries);
 
+// console.log(1 && 0);
+// console.log(1 && 5);
+// console.log(null && 5);
+// console.log(0 && 'asdasdsadadasdsa');
 
-// if (num < 49) {
-//     console.log('Error');
-// } else if (num > 100) {
-//     console.log('Много');
+// if (hamburger === 3 && cola === 1 && fries) {
+//     console.log('Все ссыты');
 // } else {
-//     console.log('Ничего');
+//     console.log('Идем в другое заведение');
 // }
 
-// (num === 50) ? console.log('Да, ровно!'): console.log('Не ровно!');
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
 
-const num = 52;
+// if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
+//     console.log('Все довольны');
+// } else {
+//     console.log('Идем в другое заведение');
+// }
 
-switch (num) {
-    case 49:
-        console.log('Неверно');
-        break;
-    case 100:
-        console.log('Неверно');
-        break;
-    case 50:
-        console.log('Верно');
-        break;
-    default:
-        console.log('Ничего не соотвествует');
+// console.log(hamburger || cola || fries);
+
+// let johnReport, alexReport, samReport, mariaReport = 'done';
+
+// console.log(johnReport || alexReport || samReport || mariaReport);
+
+// console.log(!0);
+
+console.log(NaN || 2 || undefined);
+// 2
+console.log(NaN && 2 && undefined);
+// nan
+console.log(1 && 2 && 3);
+// 3
+console.log(!1 && 2 || !3);
+// 1 = false 2 = true part1 = false  3 = false
+console.log(25 || null && !3);
+// 25
+console.log(NaN || null || !3 || undefined || 5);
+// 5
+console.log(NaN || null && !3 && undefined || 5);
+// 5
+console.log(5 === 5 && 3 > 1 || 5);
+// 
+
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+
+if (hamburger && cola || fries === 3 && nuggets) {
+    console.log('Done!');
 }
